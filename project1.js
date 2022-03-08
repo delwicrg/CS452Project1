87f1c507a7d84e3d0f0b89e08f1ab38a7732adb4
Holly Rossmann <rossmah@clarkson.edu>
2022-03-08 13:16:08 -0500

    fixed

--- project1.js
+++ project1.js
@@ -4,160 +4,165 @@
     March 11, 2022
 */
 
-    "use strict";
+"use strict";
     
-    var canvas;
-    var gl;
-    var myShaderProgram;
-    
-    var thetaAnim;
-    var thetaLoc;
-    var stopStartFlag;
+var canvas;
+var gl;
+var myShaderProgram;
 
-    var tx;
-    var ty;   
-    
-    
-    function init(){
-        canvas = document.getElementById("gl-canvas"); // Set up the canvas
+var thetaAnim = 0.0;
+var thetaLoc;
+var stopStartFlag = 0.0;
 
-        gl = WebGLUtils.setupWebGL(canvas);
-        if (!gl) { alert( "WebGL is not available" ); }
-    
-        gl.viewport( 0, 0, 700, 700 );   // set up viewport
-        
-        gl.clearColor( 0.0, 0.0, 0.0, 0); // set up background color
-    
-        myShaderProgram = initShaders( gl,"vertex-shader", "fragment-shader-spider" ); // pink
-    
-        gl.useProgram( myShaderProgram );
-    
-        gl.clear( gl.COLOR_BUFFER_BIT ); // force the webgl context to clear the color buffer
-    
-        thetaAnim = 0.0;
-        stopStartFlag = 0;
-        tx = 0.0;
-        ty = 0.0;
-    
-        var mouseCoordinatesUniform = gl.getUniformLocation(myShaderProgram, "mouseCoordinates");
-        gl.uniform2f(mouseCoordinatesUniform,tx,ty);
-    
-    
-        setUpSpider();
+var tx = 0.0;
+var ty = 0.0;
 
-       //drawSpider();
+var flagX = 0;
+var flagY = 0;
+    
+var offset = 0.01;
+
+const INCREMENT  = .001; 
+
+
+
+
+function init(){
+    // Set up the canvas
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert( "WebGL is not available" ); }
+
+    gl.viewport( 0, 0, 700, 700 );   // set up viewport
+    
+    gl.clearColor( 1.0, 0.0, 0.0, 1.0); // set up background color
+
+
+    myShaderProgram = initShaders( gl,"vertex-shader", "fragment-shader-spider" ); // pink
+    gl.useProgram( myShaderProgram );
+
+
+
+    gl.clear( gl.COLOR_BUFFER_BIT ); // force the webgl context to clear the color buffer
+
+    thetaAnim = 0.0;
+    stopStartFlag = 0.0;
+    tx = 0.0;
+    ty = 0.0;
+
+
+
+    var mouseCoordinatesUniform = gl.getUniformLocation(myShaderProgram, "mouseCoordinates");
+    gl.uniform2f(mouseCoordinatesUniform,tx,ty);
+
+
+    setUpSpider();
+
+    requestAnimFrame(drawSpider);
+}
+
+function moveSquareKeys( event ){
+    var theKeyCode = event.keyCode;
+    offset;     
+   
+    if(theKeyCode == 65){           // a, move left
+        tx -= offset;
+        flagX = -1.0;
+        flagY = 0.0;
+    } 
+    else if(theKeyCode == 68 ){     // d, move right
+        tx += offset;
+        flagX = 1.0;
+        flagY = 0.0;
+    } 
+    else if(theKeyCode == 87 ){     // w, move up
+        ty += offset;
+        flagX = 0.0;
+        flagY = 1.0;
     }
-        
-    function setUpSpider(){
-        console.log("begin setup");
-        var arrayOfPoints=[];
-
-        var theta = 0.0;
-        var x = 0.0;
-        var y = 0.0;
-        var n = 64;
-        var thetastep = 2.0 * Math.PI/n;
-        var i = 0;
-
-        var myPoint;
-
-        for( i = 0; i < n; i++) {
-            theta = i * thetastep;
-            x = 0.08*Math.cos(theta); // Form: x = c cos(theta) + a (a would offset from origin)
-            y = 0.08*Math.sin(theta); // Form: y = d sin(theta) + b
-            myPoint = vec2(x,y);
-            arrayOfPoints.push(myPoint);
-            console.log("point (" + x + " , "+ y + ") added");
-        }
-
-        var bufferId = gl.createBuffer();
-        gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-        gl.bufferData( gl.ARRAY_BUFFER,
-                      flatten(arrayOfPoints), gl.STATIC_DRAW );
-       
-        var myPosition = gl.getAttribLocation( myShaderProgram, "myPosition" );
-        gl.vertexAttribPointer( myPosition, 2, gl.FLOAT, false, 0, 0 );
-        gl.enableVertexAttribArray( myPosition );
-    
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
-        console.log("end setup");
-    }
-    
-
-    /*
-   function increase(){
-    speed = speed * 1.75;
+    else if(theKeyCode == 83 ){     // s, move down
+        ty -= offset;
+        flagX = 0.0;
+        flagY = -1.0;
     }
 
-    function decrease(){
-        speed = speed * .75;
-    }
-
-    function startRotate(){
-        stopStartFlag = 1;   
-    }
-
-    function stopRotate(){
-        stopStartFlag = 0;   
-    }
-    */
+    var mouseCoordinatesUniform = gl.getUniformLocation(myShaderProgram, "mouseCoordinates");
+    gl.uniform2f(mouseCoordinatesUniform,tx,ty);
+}
 
 
-
-/*
-    function moveSpider(event){
-        var theKeyCode = event.keyCode;
-        var offset = 0.1;
-        if (theKeyCode==65) {       //a
-            console.log("a key hit");
-            direction = "left";
-        } else if(theKeyCode==68){  //d
-            console.log("d key hit");
-            direction = "right";
-        } else if(theKeyCode==83){  //s
-            console.log("s key hit");
-            direction="down";
-        } else if (theKeyCode ==87) {  //w
-            console.log("w key hit");
-            direction = "up";
-        }
-        var mouseCoordinatesUniform = gl.getUniformLocation(myShaderProgram, "mouseCoordinates");
-        gl.uniform2f(mouseCoordinatesUniform, tx, ty);
-    }
-
-    */
-  
-  
-  /*  function drawSpider(){
-        gl.clear( gl.COLOR_BUFFER_BIT ); // force the webgl context to clear the color buffer
-        thetaAnim += .03 * stopStartFlag;
-        var thetaUniform = gl.getUniformLocation(myShaderProgram, "theta");
-
-        if(direction == "left"){
-            tx = tx - speed;
-        }else if(direction=="right"){
-            tx = tx + speed;
-        }else if(direction=="down"){
-            ty = ty - speed;
-        }else if(direction=="up"){
-            ty = ty + speed;
-        }
-
-        gl.uniform1f(thetaUniform, thetaAnim)
     
-        gl.drawArrays(gl.TRIANGLE_FAN,0,6);
+function setUpSpider(){
+    var p0 = vec2(-.1,0);
+    var p1 = vec2(-.05, .1);
+    var p2 = vec2(.05, .1);
+    var p3 = vec2(.1, 0);
+    var p4 = vec2(.05, -.1);
+    var p5 = vec2(-.05, -.1);
+
+    var arrayOfPoints = [p0,p1,p2,p3,p4,p5];
+
+
+     // Create a buffer on the graphics card and send array to the buffer for use in the shaders
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER,
+                  flatten(arrayOfPoints), gl.STATIC_DRAW );  
     
-        requestAnimFrame(drawSpider);
+    // Create a pointer that iterates over the array of points in the shader code
+    var myPosition = gl.getAttribLocation( myShaderProgram, "myPosition" );
+    gl.vertexAttribPointer( myPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray( myPosition );
+     
+    // Force a draw of the triangle using the 'drawArrays()' call
+    
+}
+
+function moveSpider(event){
+    var canvasX = event.clientX;
+    var canvasY = event.clientY;
+
+    tx =  2.0 * canvasX/700.0 - 1;
+    ty = -2.0 * canvasY/700.0 + 1;
+
+    var mouseCoordinatesUniform = gl.getUniformLocation(myShaderProgram, "mouseCoordinates");
+    gl.uniform2f(mouseCoordinatesUniform,tx,ty);
+
+}
+
+
+function startRotate(){ stopStartFlag = 1;}
+function stopRotate(){ stopStartFlag = 0.0;}
+
+
+function incSpeed(){ offset += INCREMENT;}
+
+function decSpeed(){ 
+    if((offset - INCREMENT) >= 0){
+        offset -= INCREMENT;
     }
-*/
+    else{ offset = 0;}
+}    
 
-/*
-    function drawAnts(){
 
-    }
+function drawSpider(){
+    gl.clear( gl.COLOR_BUFFER_BIT ); // force the webgl context to clear the color buffer
 
-    function moveAnts(){
+    thetaAnim += .03 * stopStartFlag;
+    var thetaUniform = gl.getUniformLocation(myShaderProgram, "theta");
+    gl.uniform1f(thetaUniform, thetaAnim)
 
-    }
+    gl.drawArrays(gl.TRIANGLE_FAN,0,6);
 
-    */
+    requestAnimFrame(drawSpider);
+}
+
+
+
+function drawAnts(){
+
+}
+
+function moveAnts(){
+
+}
